35b6c2de73d634c39f519f6c4cae7f7a42c82c84
Lewis <lewisjohnbaxter@gmail.com>
2023-12-14 16:32:53 +0000

    init

---
+++ ExampleProject/ExampleClass.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ExampleProject;
+
+public class ExampleClass
+{
+    private readonly ILogger<ExampleClass> _logger;
+
+    public ExampleClass(ILogger<ExampleClass> logger)
+    {
+        _logger = logger;
+    }
+
+    public int MultiplyValues(Example data)
+    {
+        try
+        {
+            data.Result = data.ValueOne * data.ValueTwo;
+
+            return data.Result;
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical(e, $"Failed to multiple {data.ValueOne} by {data.ValueTwo}");
+            throw;
+        }
+    }
+
+    public class Example
+    {
+        public int ValueOne { get; set; }
+        public int ValueTwo { get; set; }
+        public int Result { get; set; }
+    }
+}
---
+++ UnitTests/ExampleTest.cs
@@ -0,0 +1,61 @@
+﻿using ExampleProject;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+using static ExampleProject.ExampleClass;
+
+namespace UnitTests;
+
+public class ExampleTest
+{
+    private ExampleClass _sut;
+    private ILogger<ExampleClass> _loggerMock = Substitute.For<ILogger<ExampleClass>>();
+
+    public ExampleTest()
+    {
+        _sut = new ExampleClass(_loggerMock);
+    }
+
+    [Fact]
+    public void MultiplyValues_ShouldLogCriticalAndThrow_WhenNullEntityProvided()
+    {
+        // Arrange
+        Example entity = null;
+
+        var expectedMessage = "Failed to multiple 0 by 0";
+
+        // Act
+        var resultAction = () => _sut.MultiplyValues(entity);
+
+        // Assert
+        resultAction.Should().Throw<NullReferenceException>();
+        
+        _loggerMock.Received().Log(LogLevel.Critical, expectedMessage);
+    }
+
+    [Theory]
+    [InlineData(5, 20, 100)]
+    [InlineData(2, 31, 62)]
+    [InlineData(6, 8, 48)]
+    public void MultiplyValues_ShouldReturnCorrectResult_WhenTestCaseCorrect(int valueOne, int valueTwo, int result)
+    {
+        // Arrange
+        var testCase = new Example
+        {
+            ValueOne = valueOne,
+            ValueTwo = valueTwo
+        };
+
+        var expectedOutput = new Example
+        {
+            Result = result
+        };
+
+        // Act
+        var results = _sut.MultiplyValues(testCase);
+
+        // Assert
+        results.Should().Be(expectedOutput.Result);
+    }
+}
